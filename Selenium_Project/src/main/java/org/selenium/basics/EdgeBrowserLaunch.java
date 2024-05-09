7058e3fdc19001c2a85fc563e84237efa1cc44b2
user <user@DESKTOP-39H5R20>
2024-05-09 21:49:58 +0530

    Browser command

--- Selenium_Project/src/main/java/org/selenium/basics/EdgeBrowserLaunch.java
+++ Selenium_Project/src/main/java/org/selenium/basics/EdgeBrowserLaunch.java
@@ -10,6 +10,16 @@ public class EdgeBrowserLaunch {
 		WebDriver driver = new EdgeDriver();
 		driver.get("https://www.seleniumeasy.com/");
 		driver.manage().window().maximize();
+		String title =driver.getTitle();
+		System.out.println(title);
+		String currenturl =driver.getCurrentUrl();
+		System.out.println(currenturl);
+		String windowhandle=driver.getWindowHandle();
+		System.out.println(windowhandle);
+		String pagesource=driver.getPageSource();
+		System.out.println(pagesource);
+		driver.close();
+		
 		
 
 	}
