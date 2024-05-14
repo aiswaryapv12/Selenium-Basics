c4b7e79295b1841a2fef3dce0d3dca30359576f6
user <user@DESKTOP-39H5R20>
2024-05-14 19:55:56 +0530

    partial libk, link, css selector, xpath

--- Selenium_Project/src/main/java/org/selenium/basics/Commands.java
+++ Selenium_Project/src/main/java/org/selenium/basics/Commands.java
@@ -1,4 +1,6 @@
 package org.selenium.basics;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,7 +22,7 @@ public class Commands {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().window().maximize();
-		WebElement user_namefield = driver.findElement(By.id("user-name"));
+		WebElement user_namefield = driver.findElement(By.id("user-name")); 
 		user_namefield.sendKeys("standard_user");
 		WebElement password_field = driver.findElement(By.id("password"));
 		password_field.sendKeys("secret_sauce");
@@ -57,6 +59,60 @@ public class Commands {
 		 
 	 }
 	 
+	 public void verifyLinkText()
+	 {
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://demo.guru99.com/test/newtours/index.php");
+		 driver.manage().window().maximize();
+		 WebElement destination_field = driver.findElement(By.linkText("your destination"));
+		 destination_field.click();
+	 }
+	 public void verifyPartialLinkText()
+	 {
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://demo.guru99.com/test/newtours/index.php");
+		 driver.manage().window().maximize();
+		 WebElement destination_field = driver.findElement(By.partialLinkText(" destination"));
+		 destination_field.click();
+	 }
+	 
+	 
+	 public void totalNoOfTagInTheApplication()
+	 {
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://demo.guru99.com/test/newtours/index.php");
+		 driver.manage().window().maximize();
+		 List<WebElement> inputtype =driver.findElements(By.tagName("td"));
+		 int inputsize =inputtype.size();
+		 System.out.println(" total no input tag is "+inputsize);
+	 }
+	 
+	 public void verifyXpath()
+	 {
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://demo.guru99.com/test/newtours/index.php");
+		 driver.manage().window().maximize();
+		 WebElement username_field =driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input"));
+		 username_field.sendKeys("aiswarya");
+		 WebElement password_field =driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[3]/td[2]/input"));
+		 password_field.sendKeys("Aiswarya");
+		 WebElement submit_button =driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[4]/td[2]/div/input"));
+		 submit_button.click();
+	 }
+	 
+	 public void verifyLoginByCss()
+	 {
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://demo.guru99.com/test/newtours/index.php");
+		 driver.manage().window().maximize();
+		 WebElement username_field =driver.findElement(By.cssSelector("body > div:nth-child(5) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(3) > form > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=text] "));
+		 username_field.sendKeys("aiswarya");
+		 WebElement password_field =driver.findElement(By.cssSelector("body > div:nth-child(5) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(3) > form > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(3) > td:nth-child(2) > input[type=password]"));
+		 password_field.sendKeys("Aiswarya");
+		 WebElement submit_button =driver.findElement(By.cssSelector("body > div:nth-child(5) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(3) > form > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(4) > td:nth-child(2) > div > input[type=submit]"));
+		 submit_button.click();
+	 }
+	 
 	public static void main(String[] args) {
 	
 		Commands obj = new Commands();
@@ -64,7 +120,12 @@ public class Commands {
 	
 		//obj.verifySwapLapLogin();
 		//obj.verifySwapLoginname();
-		obj.verifySwapLoginClassName();
+		//obj.verifySwapLoginClassName();
+		//obj.verifyLinkText();
+		//obj.verifyPartialLinkText();
+		//obj.totalNoOfTagInTheApplication();
+		//obj.verifyXpath();
+		obj.verifyLoginByCss();
 
 	}
 
