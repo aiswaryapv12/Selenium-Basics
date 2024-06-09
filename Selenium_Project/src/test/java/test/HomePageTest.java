3097a2b7fdbe6a5c1f529c1cc790cfc0ecd8b24c
aiswaryapv12 <aiswaryasankaran123@gmail.com>
2024-06-10 01:50:15 +0530

    Homepage

--- Selenium_Project/src/test/java/test/HomePageTest.java
+++ Selenium_Project/src/test/java/test/HomePageTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -8,31 +9,34 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import automation_core.Base;
+import utilities.Excel_Utility;
 
 public class HomePageTest extends Base {
 	
 	@Test
-	public void verifyHomePageTitle()
+	public void verifyHomePageTitle() throws IOException
 	{
 		driver.get("https://demowebshop.tricentis.com/");
 		String actualtitle =driver.getTitle();
 		System.out.println(" title of the page "+actualtitle);
-		String expectedtitle= "Demo Web Shop";
+		String expectedtitle=Excel_Utility.readStringData(0, 0,"Home_Page");
 		Assert.assertEquals(actualtitle, expectedtitle,"Invalid title");
 		
 	}
 	
 	@Test
-	public void verifyCommunityPollSelection()
+	public void verifyCommunityPollSelection() throws IOException
 	{
 		driver.get("https://demowebshop.tricentis.com/");
 		List<WebElement> checkbox_field =driver.findElements(By.xpath("//li[@class='answer']//input[@name ='pollanswers-1']//following-sibling::label"));
+		
+		
 		for(WebElement radiobutton: checkbox_field)
 		{
 			String text = radiobutton.getText();
 			System.out.println("options "+text);
-			
-			if(text.equalsIgnoreCase("Good"))
+			String data =Excel_Utility.readStringData(0, 1,"Home_Page");
+			if(text.equalsIgnoreCase(data))
 			{
 				radiobutton.click();
 				
